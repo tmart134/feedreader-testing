e73272427622de423e18a1fd1272bf46b532b498
Tracey Martin <martinltracey@gmail.com>
2018-10-17 07:51:28 -0400

    menu open close test added jasmime-jquery

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -2,25 +2,23 @@
  *
  * This is the spec file that Jasmine will read and contains
  * all of the tests that will be run against your application.
+
+ *Resources : 
+ *https://jasmine.github.io/2.0/introduction.html
+ *https://github.com/velesin/jasmine-jquery
+
  */
 
-/* We're placing all of our tests within the $() function,
+/* All of the tests are within the $() function,
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+    /* This suite is all about the RSS
+     * feeds definitions, the allFeeds variable in our application.
+     */
    describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
+     
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
@@ -33,7 +31,6 @@ $(function() {
             }
          });
 
-
         it('should have a name', function(){
             for(let feed of allFeeds){
                 expect(feed.name).toBeDefined();
@@ -43,10 +40,54 @@ $(function() {
          
     });
 
-   
-      
+   describe('The menu', function(){
 
+        it('should be hidden by default', function(){
+
+            let menu = $( 'body' );
+            expect(menu.hasClass('menu-hidden')).toBe(true);
+
+         });
+
+        it('should open when clicked', function(){
+
+            var spyEvent = spyOnEvent('.menu-icon-link', 'click');
+            $('.menu-icon-link').click();
+            expect('click').toHaveBeenTriggeredOn('.menu-icon-link');
+            expect(spyEvent).toHaveBeenTriggered();
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+        });
+            
+
+       it ('should hide when clicked again', function(){    
+            
+            var spyEvent = spyOnEvent('.menu-icon-link', 'click');
+            $('.menu-icon-link').click();
+            expect('click').toHaveBeenTriggeredOn('.menu-icon-link');
+            expect(spyEvent).toHaveBeenTriggered();
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
+
+    }); 
+      
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function(){
+
+        beforeEach(function(done){
+            loadFeed(0,function(){
+                
+            }
+        })
+
+        it('should have at least 1 entry', function(){
+
+
+            expect($('<div class="feed"></div>')).toContainElement('article.entry');
+        
+        })
+
+
+    })
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
