d7612098ab90b9cf57cdcac113464199e835f7a4
Tracey Martin <martinltracey@gmail.com>
2018-10-17 15:06:17 -0400

    At least 1 feed is loaded

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -69,32 +69,27 @@ $(function() {
         });
 
     }); 
-      
-    /* TODO: Write a new test suite named "Initial Entries" */
+
     describe('Initial Entries', function(){
 
-        beforeEach(function(done){
-            loadFeed(0,function(){
-                
-            }
-        })
+        beforeEach(function(done) {
+            
+            loadFeed(0, function() {
+                    done();
+                });
+           
+        });
+
 
         it('should have at least 1 entry', function(){
 
-
-            expect($('<div class="feed"></div>')).toContainElement('article.entry');
+            var firstEntry = $('.entry')[0];
+            expect(firstEntry).toBeInDOM();
         
         })
 
-
-    })
-
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+            
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
